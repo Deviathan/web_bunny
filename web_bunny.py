1cc266a48b0ba98761824ed91e1d021eb5413a27
Deviathan <deviathangr@gmail.com>
2018-10-17 11:55:29 +0300

    Begin of the Project

---
+++ web_bunny.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template 
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/target')
+def target():
+    return render_template('target.html')
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+@app.route("/options")
+def options():
+    return render_template("options.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('internal_error.html'),500
+
+if __name__ == '__main__':
+    app.run(debug="True")
+
+
+    
