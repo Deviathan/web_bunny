f7eb4aaf5af68de19ee97fa83225aeb9ea5fc556
Deviathan <deviathangr@gmail.com>
2018-10-21 15:07:36 +0300

    added forms

--- forms.py
+++ forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,BooleanField
+from wtforms import StringField,SubmitField,BooleanField,SelectField
 from wtforms.validators import DataRequired
 
 class url_form(FlaskForm):
+    req = SelectField('Select REQ type:', choices = [('POST','POST'),('GET','GET'),('PUT','PUT'),('DELETE','DELETE'),('HEAD','HEAD'),('OPTIONS','OPTIONS')])    
     url = StringField('URL' ,validators = [DataRequired()])
-    c_ua = BooleanField("Want to change User-Agent?")
-    execute = SubmitField('Execute') 
+    c_ua = SelectField('Change User Agent?', choices = [('NO','NO'),('YES','YES')])
+    in_ua = StringField('Insert User Agent?',validators = [DataRequired()])
+
+    execute = SubmitField('Execute')
