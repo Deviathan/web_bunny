0346e9371046a9168ee5bd5b607057f6755a16ec
dev <dev@localhost.localdomain>
2018-10-31 02:40:59 +0200

    user_agent bug fixed

--- web_bunny.py
+++ web_bunny.py
@@ -29,62 +29,62 @@ app.config.update(dict(
 def index():
     return render_template("index.html",version=version)
 
-@app.route('/target' ,methods = ['POST','GET'])
-def target():
-    #gets input from forms (forms.py)/target.html
+@app.route('/http_requester' ,methods = ['POST','GET'])
+def http_requester():
+    #gets input from forms (forms.py)/http_requester.html
     form = url_form()
     #if Execute button is pressed runs
     if form.validate_on_submit():
         request_type = form.req.data
         url = form.url.data
         cua = form.c_ua.data
-        inua = form.c_ua.data
+        inua = form.in_ua.data
         global error , request_headers , request_reply , request_text , error_reply
 
         #calling post request (post.py)
         if (request_type=='POST'):
             r,request_headers,request_reply,request_text,error,error_reply=post(url,cua,inua)
             if (error == True):
-                return render_template('target.html',form=form,r=error_reply)
+                return render_template('httP_requester.html',form=form,r=error_reply)
             elif(error == False):
-                return render_template('target.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
+                return render_template('http_requester.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
         #calling get request (get.py)
         if (request_type=='GET'):       
             r,request_headers,request_reply,request_text,error,error_reply=get(url,cua,inua)
             if (error == True):
-                return render_template('target.html',form=form,r=error_reply)
+                return render_template('http_requester.html',form=form,r=error_reply)
             elif(error == False):
-                return render_template('target.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
+                return render_template('http_requester.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
         #calling put request (put.py)
         if (request_type=='PUT'):
             r = put(url,cua,inua)
             r,request_headers,request_reply,request_text,error,error_reply=put(url,cua,inua)
             if (error == True):
-                return render_template('target.html',form=form,r=error_reply)
+                return render_template('http_requester.html',form=form,r=error_reply)
             elif(error == False):
-                return render_template('target.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
+                return render_template('http_requester.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
         #calling delete request (delete.py)
         if (request_type=='DELETE'):
             r,request_headers,request_reply,request_text,error,error_reply= delete(url,cua,inua)
             if (error == True):
-                return render_template('target.html',form=form,r=error_reply)
+                return render_template('http_requester.html',form=form,r=error_reply)
             elif(error == False):
-                return render_template('target.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
+                return render_template('http_requester.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
         #calling head request (head.py)
         if (request_type=='HEAD'):
             r,request_headers,request_reply,request_text,error,error_reply= head(url,cua,inua)
             if (error == True):
-                return render_template('target.html',form=form,r=error_reply)
+                return render_template('http_requester.html',form=form,r=error_reply)
             elif(error == False):
-                return render_template('target.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
+                return render_template('http_requester.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
         #calling options request (option.py)
         if (request_type=='OPTIONS'):
             r,request_headers,request_reply,request_text,error,error_reply= option(url,cua,inua)
             if (error == True):
-                return render_template('target.html',form=form,r=error_reply)
+                return render_template('http_requester.html',form=form,r=error_reply)
             elif(error == False):
-                return render_template('target.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
-    return render_template('target.html',form=form,r=error)
+                return render_template('http_requester.html',r=r,form=form, headers = request_headers, reply = request_reply ,text = request_text, url=url, req=request_type)
+    return render_template('http_requester.html',form=form,r=error)
 
 
 @app.route("/history")
